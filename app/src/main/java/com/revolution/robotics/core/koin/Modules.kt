5b31fe142bb7c4eb829a1411da5ca0cd6fbc57dd
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-01 15:18:04 +0200

    Removed ConnectivityHandler

--- app/src/main/java/com/revolution/robotics/core/koin/Modules.kt
+++ app/src/main/java/com/revolution/robotics/core/koin/Modules.kt
@@ -1,6 +1,5 @@
 package com.revolution.robotics.core.koin
 
-import com.revolution.robotics.core.utils.ConnectivityHandler
 import com.revolution.robotics.core.utils.dynamicPermissions.DynamicPermissionHandler
 import org.koin.dsl.module.module
 
