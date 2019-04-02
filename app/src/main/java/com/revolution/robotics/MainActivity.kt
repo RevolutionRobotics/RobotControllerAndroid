2e2cf7144686d27654c97c0e54cf38acbee8ecda
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-02 13:24:36 +0200

    RR-5: Upgraded kodein version

--- app/src/main/java/com/revolution/robotics/MainActivity.kt
+++ app/src/main/java/com/revolution/robotics/MainActivity.kt
@@ -3,12 +3,12 @@ package com.revolution.robotics
 import androidx.appcompat.app.AppCompatActivity
 import com.revolution.robotics.core.utils.dynamicPermissions.DynamicPermissionHandler
 import org.kodein.di.KodeinAware
-import org.kodein.di.android.closestKodein
+import org.kodein.di.android.kodein
 import org.kodein.di.erased.instance
 
 class MainActivity : AppCompatActivity(), KodeinAware {
 
-    override val kodein by closestKodein()
+    override val kodein by kodein()
     private val dynamicPermissionHandler: DynamicPermissionHandler by instance()
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) =
