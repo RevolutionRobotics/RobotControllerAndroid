9af4a8f12aca4cf21f921a286018607595a9e92a
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-04 12:17:42 +0200

    Created POC for javascript bridging

--- blockly/src/main/java/org/revolution/blockly/view/BlocklyView.kt
+++ blockly/src/main/java/org/revolution/blockly/view/BlocklyView.kt
@@ -2,19 +2,36 @@ package org.revolution.blockly.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.widget.Toast
 
 @Suppress("SetJavaScriptEnabled")
 class BlocklyView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     WebView(context, attrs, defStyleAttr) {
 
     companion object {
-        private const val ASSET_PATH = "file:///android_asset/blockly/webview.html"
+        // TODO remove POC implementation
+        // private const val ASSET_PATH = "file:///android_asset/blockly/webview.html"
+        private const val ASSET_PATH = "file:///android_asset/poc.html"
     }
 
     init {
         webChromeClient = BlocklyWebChromeClient()
         settings.javaScriptEnabled = true
         loadUrl(ASSET_PATH)
+
+        addJavascriptInterface(JsInterface(), "NativePoc")
+    }
+
+    inner class JsInterface {
+
+        @JavascriptInterface
+        fun createMessage(input: String) =
+            "$input World"
+
+        @JavascriptInterface
+        fun displayMessage(input: String) =
+            Toast.makeText(context, input, Toast.LENGTH_LONG).show()
     }
 }
