89d440e2754bdea3d874c6d7af90c8d505963d83
László Nagy <laszlo.nagy@supercharge.io>
2019-04-05 08:59:25 +0200

    Added support for firebase images
    RR-10

--- app/src/main/java/com/revolution/robotics/core/bindings/GlideBindings.kt
+++ app/src/main/java/com/revolution/robotics/core/bindings/GlideBindings.kt
@@ -2,11 +2,19 @@ package com.revolution.robotics.core.bindings
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.google.firebase.storage.StorageReference
 import com.revolution.robotics.core.glide.GlideApp
 
 @BindingAdapter("android:src")
 fun loadImage(imageView: ImageView, url: String?) {
     GlideApp.with(imageView)
         .load(url)
+        .into(imageView)
+}
+
+@BindingAdapter("firebaseImage")
+fun loadFirebaseImage(imageView: ImageView, reference: StorageReference) {
+    GlideApp.with(imageView)
+        .load(reference)
         .into(imageView)
 }
