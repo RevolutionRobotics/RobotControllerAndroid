c65b40ba612460222c960de49f10b6c885f1d6d3
László Nagy <laszlo.nagy@supercharge.io>
2019-04-08 08:48:37 +0200

    Finalized layouts
    Reimport every assets from the Android Zeplin project and refactor the fragment_menu.xml
    RR-20

--- app/src/main/java/com/revolution/robotics/mainmenu/MainMenuViewModel.kt
+++ app/src/main/java/com/revolution/robotics/mainmenu/MainMenuViewModel.kt
@@ -16,4 +16,13 @@ class MainMenuViewModel(private val navigationEventBus: NavigationEventBus) : Vi
     fun onChallengesClicked() {
         navigationEventBus.publishEvent(MainMenuFragmentDirections.actionMainMenuFragmentToChallengesFragment())
     }
+
+    // Handle community navigation
+    fun onCommunityIconClicked() {
+    }
+
+    // Handle robotics icon navigation
+    fun onRoboticsIconClicked() {
+    }
+
 }
