bace8e424e0be00a56f1a46f79503763c12226e1
adam.hosszu <adam.hosszu@supercharge.io>
2019-04-16 09:28:34 +0200

    RR-30 Create chipped edge drawable
    RR-30

--- app/src/main/java/com/revolution/robotics/core/bindings/CustomBindings.kt
+++ app/src/main/java/com/revolution/robotics/core/bindings/CustomBindings.kt
@@ -1,9 +1,20 @@
 package com.revolution.robotics.core.bindings
 
+import android.view.View
 import android.widget.SeekBar
 import androidx.databinding.BindingAdapter
+import com.revolution.robotics.core.utils.chippedBox.ChippedBoxConfig
+import com.revolution.robotics.core.utils.chippedBox.ChippedBoxDrawable
 
 @BindingAdapter("listener")
 fun setSeekbarListener(seekBar: SeekBar, listener: SeekBar.OnSeekBarChangeListener) {
     seekBar.setOnSeekBarChangeListener(listener)
+}
+
+@BindingAdapter("chippedBoxConfig")
+fun setChippedBoxConfig(
+    view: View,
+    config: ChippedBoxConfig
+) {
+    view.background = ChippedBoxDrawable(view.context, config)
 }
