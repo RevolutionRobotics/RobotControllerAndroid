7d84d72a072f89ca80d1797de92612ccb145cc18
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-18 13:57:04 +0200

    Screen design
    Created screen design
    
    RAD-34

--- app/src/main/java/com/revolution/robotics/myRobots/MyRobotsViewModel.kt
+++ app/src/main/java/com/revolution/robotics/myRobots/MyRobotsViewModel.kt
@@ -2,7 +2,4 @@ package com.revolution.robotics.myRobots
 
 import androidx.lifecycle.ViewModel
 
-class MyRobotsViewModel : ViewModel() {
-
-    val availableRobots = listOf<Any>()
-}
+class MyRobotsViewModel : ViewModel()
