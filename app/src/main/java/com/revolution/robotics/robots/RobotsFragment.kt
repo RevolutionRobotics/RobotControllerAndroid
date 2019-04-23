8139f528e7b4de5e599c433faf417cde844bcdfa
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-23 09:14:14 +0200

    Minor code format fixes

--- app/src/main/java/com/revolution/robotics/robots/RobotsFragment.kt
+++ app/src/main/java/com/revolution/robotics/robots/RobotsFragment.kt
@@ -3,7 +3,6 @@ package com.revolution.robotics.robots
 import android.os.Bundle
 import android.view.View
 import androidx.viewpager.widget.ViewPager
-
 import com.revolution.robotics.BaseFragment
 import com.revolution.robotics.R
 import com.revolution.robotics.core.extensions.waitForLayout
@@ -13,12 +12,12 @@ import com.revolution.robotics.robots.adapter.RobotsPagerAdapter
 import org.kodein.di.erased.instance
 import kotlin.math.floor
 
-private const val VIEWPAGER_PADDING_MULTIPLIER = 0.3125
-
+@Suppress("UnnecessaryApply")
 class RobotsFragment : BaseFragment<FragmentRobotsBinding, RobotsViewModel>(R.layout.fragment_robots), RobotsMvp.View {
 
     companion object {
         private const val ROBOTS_PAGER_OFFSCREEN_PAGE_LIMIT = 3
+        private const val VIEWPAGER_PADDING_MULTIPLIER = 0.3125
     }
 
     override val viewModelClass: Class<RobotsViewModel> = RobotsViewModel::class.java
@@ -27,19 +26,14 @@ class RobotsFragment : BaseFragment<FragmentRobotsBinding, RobotsViewModel>(R.la
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
         presenter.register(this, viewModel)
-
         binding?.apply {
             adapter = RobotsPagerAdapter(view.context)
             robotsViewpager.adapter = adapter
             robotsViewpager.offscreenPageLimit = ROBOTS_PAGER_OFFSCREEN_PAGE_LIMIT
             robotsViewpager.setPageTransformer(false, RobotsPageTransformer(robotsViewpager))
             robotsViewpager.addOnPageChangeListener(RobotsOnPageChangeListener())
-
         }
-
         view.waitForLayout {
             val viewPagePadding = floor(view.width * VIEWPAGER_PADDING_MULTIPLIER).toInt()
             binding?.robotsViewpager?.setPaddingRelative(viewPagePadding, 0, viewPagePadding, 0)
@@ -55,8 +49,8 @@ class RobotsFragment : BaseFragment<FragmentRobotsBinding, RobotsViewModel>(R.la
     }
 
     inner class RobotsOnPageChangeListener : ViewPager.OnPageChangeListener {
-        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-        override fun onPageScrollStateChanged(state: Int) {}
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) = Unit
+        override fun onPageScrollStateChanged(state: Int) = Unit
 
         override fun onPageSelected(position: Int) {
             presenter.onPageSelected(position)
