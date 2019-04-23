7313b43030e60bbcac4341662373444b0f063e17
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-04-23 14:04:52 +0200

    Reworked extension to fix a crash

--- app/src/main/java/com/revolution/robotics/core/extensions/HackExtensions.kt
+++ app/src/main/java/com/revolution/robotics/core/extensions/HackExtensions.kt
@@ -3,9 +3,12 @@ package com.revolution.robotics.core.extensions
 import androidx.core.view.postDelayed
 import androidx.viewpager.widget.ViewPager
 
-fun ViewPager.forceApplyTransformer() =
-    postDelayed(50) {
+fun ViewPager.forceApplyTransformer() {
+    if (adapter?.count != 0) {
         beginFakeDrag()
         fakeDragBy(1.0f)
         endFakeDrag()
+    } else {
+        postDelayed(10, ::forceApplyTransformer)
     }
+}
