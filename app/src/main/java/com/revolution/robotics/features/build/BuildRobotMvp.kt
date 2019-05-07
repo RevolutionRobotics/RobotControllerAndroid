fd3d7035a6a9804eb946751eedbf24fb21d4e76b
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-05-07 15:04:40 +0200

    Removed unused import

--- app/src/main/java/com/revolution/robotics/features/build/BuildRobotMvp.kt
+++ app/src/main/java/com/revolution/robotics/features/build/BuildRobotMvp.kt
@@ -3,7 +3,6 @@ package com.revolution.robotics.features.build
 import com.revolution.robotics.core.Mvp
 import com.revolution.robotics.core.domain.local.UserRobot
 import com.revolution.robotics.core.domain.remote.BuildStep
-import com.revolution.robotics.core.domain.remote.Robot
 import com.revolution.robotics.core.domain.shared.RobotDescriptor
 
 interface BuildRobotMvp : Mvp {
