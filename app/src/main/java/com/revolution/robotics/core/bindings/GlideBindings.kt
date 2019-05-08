a73a62a16fd98bb3971aa9318b32e217fa7182e7
adam.hosszu <adam.hosszu@supercharge.io>
2019-05-08 11:43:42 +0200

    [RAD-69] Add error image placeholder
    RAD-69

--- app/src/main/java/com/revolution/robotics/core/bindings/GlideBindings.kt
+++ app/src/main/java/com/revolution/robotics/core/bindings/GlideBindings.kt
@@ -1,45 +1,75 @@
 package com.revolution.robotics.core.bindings
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.revolution.robotics.R
 import com.revolution.robotics.core.glide.GlideApp
 import com.revolution.robotics.views.RemoteImageView
 
-@BindingAdapter("android:src")
-fun loadImage(imageView: ImageView, url: String?) {
+@BindingAdapter("android:src", "errorDrawable", requireAll = false)
+fun loadImage(imageView: ImageView, url: String?, errorDrawable: Drawable?) {
     if (!url.isNullOrEmpty()) {
         GlideApp.with(imageView)
             .load(url)
+            .apply {
+                if (errorDrawable == null) {
+                    error(R.drawable.ic_image_not_found)
+                } else {
+                    error(errorDrawable)
+                }
+            }
             .into(imageView)
     }
 }
 
-@BindingAdapter("localResource")
-fun loadLocalResource(imageView: ImageView, @DrawableRes localResource: Int) {
+@BindingAdapter("localResource", "errorDrawable", requireAll = false)
+fun loadLocalResource(imageView: ImageView, @DrawableRes localResource: Int, errorDrawable: Drawable?) {
     GlideApp.with(imageView)
         .load(localResource)
+        .apply {
+            if (errorDrawable == null) {
+                error(R.drawable.ic_image_not_found)
+            } else {
+                error(errorDrawable)
+            }
+        }
         .into(imageView)
 }
 
-@BindingAdapter("firebaseImage")
-fun loadFirebaseImage(imageView: ImageView, reference: StorageReference) {
+@BindingAdapter("firebaseImage", "errorDrawable", requireAll = false)
+fun loadFirebaseImage(imageView: ImageView, reference: StorageReference, errorDrawable: Drawable?) {
     GlideApp.with(imageView)
         .load(reference)
+        .apply {
+            if (errorDrawable == null) {
+                error(R.drawable.ic_image_not_found)
+            } else {
+                error(errorDrawable)
+            }
+        }
         .into(imageView)
 }
 
-@BindingAdapter("firebaseImageUrl")
-fun loadFirebaseImage(imageView: ImageView, gsUrl: String?) {
+@BindingAdapter("firebaseImageUrl", "errorDrawable", requireAll = false)
+fun loadFirebaseImage(imageView: ImageView, gsUrl: String?, errorDrawable: Drawable?) {
     if (!gsUrl.isNullOrEmpty()) {
         GlideApp.with(imageView)
             .load(FirebaseStorage.getInstance().getReferenceFromUrl(gsUrl))
+            .apply {
+                if (errorDrawable == null) {
+                    error(R.drawable.ic_image_not_found)
+                } else {
+                    error(errorDrawable)
+                }
+            }
             .into(imageView)
     }
 }
 
-@BindingAdapter("firebaseImageUrl")
-fun loadFirebaseImage(remoteImageView: RemoteImageView, gsUrl: String?) =
-    loadFirebaseImage(remoteImageView.image, gsUrl)
+@BindingAdapter("firebaseImageUrl", "errorDrawable", requireAll = false)
+fun loadFirebaseImage(remoteImageView: RemoteImageView, gsUrl: String?, errorDrawable: Drawable?) =
+    loadFirebaseImage(remoteImageView.image, gsUrl, errorDrawable)
