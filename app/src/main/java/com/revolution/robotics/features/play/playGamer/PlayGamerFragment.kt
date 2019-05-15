ff8433801028989a84b640d36b6aaee5df020f59
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-05-15 09:42:53 +0200

    Added connection logic
    RAD-115

--- app/src/main/java/com/revolution/robotics/features/play/playGamer/PlayGamerFragment.kt
+++ app/src/main/java/com/revolution/robotics/features/play/playGamer/PlayGamerFragment.kt
@@ -6,6 +6,7 @@ import com.revolution.robotics.BaseFragment
 import com.revolution.robotics.R
 import com.revolution.robotics.core.kodein.utils.ResourceResolver
 import com.revolution.robotics.databinding.FragmentPlayGamerBinding
+import com.revolution.robotics.features.bluetooth.BluetoothManager
 import com.revolution.robotics.features.play.PlayViewModel
 import com.revolution.robotics.features.play.PlayToolbarViewModel
 import org.kodein.di.erased.instance
@@ -15,8 +16,12 @@ class PlayGamerFragment : BaseFragment<FragmentPlayGamerBinding, PlayViewModel>(
     override val viewModelClass = PlayViewModel::class.java
 
     private val resourceResolver: ResourceResolver by kodein.instance()
+    private val bluetoothManager: BluetoothManager by kodein.instance()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding?.toolbarViewModel = PlayToolbarViewModel(resourceResolver)
+        if (!bluetoothManager.isConnected) {
+            bluetoothManager.startConnectionFlow()
+        }
     }
 }
