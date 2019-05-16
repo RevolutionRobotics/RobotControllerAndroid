cffa7f0675acc5cd9e5d67e787d8e7ebe50dfe1f
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-05-16 23:25:01 +0200

    Codestyle fixes

--- app/src/main/java/com/revolution/robotics/features/controllers/setup/SetupFragment.kt
+++ app/src/main/java/com/revolution/robotics/features/controllers/setup/SetupFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.revolution.robotics.BR
 import com.revolution.robotics.BaseFragment
 import com.revolution.robotics.R
 import com.revolution.robotics.core.kodein.utils.ResourceResolver
