42d4cc30aa9d3235b9dfedeae28c00e9ab266673
László Nagy <laszlo.nagy@supercharge.io>
2019-05-27 10:26:03 +0200

    Created challenge detail slider view
    RAD-173

---
+++ app/src/main/java/com/revolution/robotics/features/challenges/challengeDetail/ChallengeDetailSlider.kt
@@ -0,0 +1,70 @@
+package com.revolution.robotics.features.challenges.challengeDetail
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.SeekBar
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.revolution.robotics.core.domain.remote.ChallengeStep
+import com.revolution.robotics.core.extensions.visible
+import com.revolution.robotics.databinding.SliderChallengeDetailBinding
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+
+class ChallengeDetailSlider @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+    ConstraintLayout(context, attrs), KodeinAware, SeekBar.OnSeekBarChangeListener {
+
+    override val kodein: Kodein by kodein()
+    private val binding = SliderChallengeDetailBinding.inflate(LayoutInflater.from(context), this, true).apply {
+        seekbarBuildSteps.setOnSeekBarChangeListener(this@ChallengeDetailSlider)
+        imgBack.setOnClickListener { seekbarBuildSteps.selectPrevious() }
+        imgNext.setOnClickListener { onNextClicked() }
+        imgFinish.setOnClickListener { buildStepSelectedListener?.onChallengeFinished() }
+    }
+
+    private var buildStepSelectedListener: ChallengeStepSelectedListener? = null
+    private var buildSteps: List<ChallengeStep>? = null
+
+    private fun onNextClicked() {
+        val currentBuildStep = buildSteps?.get(binding.seekbarBuildSteps.progress)
+        val listener = buildStepSelectedListener
+        if (currentBuildStep != null && listener != null) {
+            if (listener.shouldShowNext(currentBuildStep)) {
+                next()
+            }
+        } else {
+            next()
+        }
+    }
+
+    fun next() {
+        binding.seekbarBuildSteps.selectNext()
+    }
+
+    fun setChallengeSteps(steps: List<ChallengeStep>, listener: ChallengeStepSelectedListener) {
+        buildSteps = steps
+        this.buildStepSelectedListener = listener
+        binding.seekbarBuildSteps.apply {
+            setChallengeSteps(steps)
+            progress = 0
+        }
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+        binding.imgFinish.visible = buildSteps?.size == progress + 1
+        buildSteps?.get(progress)?.let { step ->
+            buildStepSelectedListener?.onChallengeStepSelected(step, fromUser)
+        }
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) = Unit
+
+    override fun onStopTrackingTouch(p0: SeekBar?) = Unit
+
+    interface ChallengeStepSelectedListener {
+        fun shouldShowNext(challengeStep: ChallengeStep): Boolean
+        fun onChallengeStepSelected(challengeStep: ChallengeStep, fromUser: Boolean)
+        fun onChallengeFinished()
+    }
+}
