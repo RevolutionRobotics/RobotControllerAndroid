f5f6b00ee04988a438ec6a9e17a6f7b60ba7454d
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-05-31 11:02:22 +0200

    Build UI can now display two parts per build step
    RAD-194

--- app/src/main/java/com/revolution/robotics/core/domain/remote/BuildStep.kt
+++ app/src/main/java/com/revolution/robotics/core/domain/remote/BuildStep.kt
@@ -3,6 +3,7 @@ package com.revolution.robotics.core.domain.remote
 data class BuildStep(
     var image: String? = null,
     var partImage: String? = null,
+    var part2Image: String? = null,
     var robotId: Int = 0,
     var stepNumber: Int = 0,
     var milestone: Milestone? = null
