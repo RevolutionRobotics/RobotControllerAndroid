cbe3dcbb2fdde1a103b5d7fe89fb30207a642b6c
László Nagy <laszlo.nagy@supercharge.io>
2019-06-05 17:39:58 +0200

    Created python code dialog

--- app/src/main/java/com/revolution/robotics/features/coding/CodingPresenter.kt
+++ app/src/main/java/com/revolution/robotics/features/coding/CodingPresenter.kt
@@ -1,7 +1,5 @@
 package com.revolution.robotics.features.coding
 
-import com.revolution.robotics.core.utils.Navigator
-
 import com.revolution.robotics.R
 import com.revolution.robotics.core.domain.local.UserProgram
 import com.revolution.robotics.core.interactor.LocalFileLoader
@@ -9,9 +7,12 @@ import com.revolution.robotics.core.interactor.LocalFileSaver
 import com.revolution.robotics.core.interactor.RemoveUserProgramInteractor
 import com.revolution.robotics.core.interactor.SaveUserProgramInteractor
 import com.revolution.robotics.core.kodein.utils.ResourceResolver
+import com.revolution.robotics.core.utils.Navigator
 import com.revolution.robotics.core.utils.UserProgramFileNameGenerator
 import com.revolution.robotics.features.coding.programs.ProgramsDialog
+import com.revolution.robotics.features.coding.python.PythonDialog
 import com.revolution.robotics.features.coding.saveProgram.SaveProgramDialog
+import org.revolution.blockly.view.jsInterface.BlocklyJavascriptListener
 
 class CodingPresenter(
     private val removeUserProgramInteractor: RemoveUserProgramInteractor,
@@ -87,6 +88,18 @@ class CodingPresenter(
                 saveUserProgramWhenEveryDataIsReady()
             }
         }
+    }
+
+    override fun showPythonCode() {
+        view?.getPythonCodeFromBlockly(object : BlocklyJavascriptListener {
+            override fun onPythonProgramSaved(file: String) {
+                view?.showDialog(PythonDialog.newInstance(file))
+            }
+
+            override fun onXMLProgramSaved(file: String) = Unit
+
+            override fun onVariablesExported(variables: String) = Unit
+        })
     }
 
     override fun onVariablesExported(variables: String) {
