6c64b6a0a0cdc26c43097a1a6609d1ab8301712a
Szabolcs Nagy <szabolcs.nagy@supercharge.io>
2019-06-14 11:55:57 +0200

    Updated date format
    RAD-203

--- app/src/main/java/com/revolution/robotics/core/extensions/DateExtensions.kt
+++ app/src/main/java/com/revolution/robotics/core/extensions/DateExtensions.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 fun Date.formatYearMonthDay(locale: Locale = Locale.getDefault()): String =
-    SimpleDateFormat("yyyy-MM-dd", locale).format(this)
+    SimpleDateFormat("yyyy-MMM-dd", locale).format(this).toUpperCase()
 
 fun Long.formatYearMonthDay(locale: Locale = Locale.getDefault()): String =
     Date(TimeUnit.SECONDS.toMillis(this)).formatYearMonthDay(locale)
