211326996e26daba50c4f04b08afc535b14477a9
László Nagy <laszlo.nagy@supercharge.io>
2019-06-25 14:27:44 +0200

    Added program init to the splash screen
    RAD-193

--- app/src/main/java/com/revolution/robotics/core/interactor/SaveUserProgramsInteractor.kt
+++ app/src/main/java/com/revolution/robotics/core/interactor/SaveUserProgramsInteractor.kt
@@ -1,13 +1,11 @@
 package com.revolution.robotics.core.interactor
 
-import com.revolution.robotics.core.domain.local.ProgramLocalFiles
 import com.revolution.robotics.core.domain.local.UserProgram
 import com.revolution.robotics.core.domain.local.UserProgramDao
 import com.revolution.robotics.core.domain.remote.Program
 
 class SaveUserProgramsInteractor(private val userProgramDao: UserProgramDao) : Interactor<Unit>() {
 
-    lateinit var programLocalFiles: List<ProgramLocalFiles>
     lateinit var programs: List<Program>
 
     override fun getData() {
@@ -17,8 +15,8 @@ class SaveUserProgramsInteractor(private val userProgramDao: UserProgramDao) : I
                     remoteProgram.description,
                     remoteProgram.lastModified,
                     remoteProgram.name ?: "",
-                    programLocalFiles.find { it.remoteId == remoteProgram.id }?.python?.path ?: "",
-                    programLocalFiles.find { it.remoteId == remoteProgram.id }?.xml?.path ?: "",
+                    remoteProgram.python ?: "",
+                    remoteProgram.xml ?: "",
                     remoteProgram.variables,
                     remoteProgram.id
                 )
--- app/src/main/java/com/revolution/robotics/features/splash/SplashPresenter.kt
+++ app/src/main/java/com/revolution/robotics/features/splash/SplashPresenter.kt
@@ -2,9 +2,13 @@ package com.revolution.robotics.features.splash
 
 import androidx.lifecycle.ViewModel
 import com.revolution.robotics.core.interactor.FirebaseInitInteractor
+import com.revolution.robotics.core.interactor.SaveUserProgramsInteractor
+import com.revolution.robotics.core.interactor.firebase.ProgramsInteractor
 
 class SplashPresenter(
-    private val firebaseInitInteractor: FirebaseInitInteractor
+    private val firebaseInitInteractor: FirebaseInitInteractor,
+    private val programsInteractor: ProgramsInteractor,
+    private val saveUserProgramsInteractor: SaveUserProgramsInteractor
 ) : SplashMvp.Presenter {
 
     override var view: SplashMvp.View? = null
@@ -13,7 +17,11 @@ class SplashPresenter(
     override fun register(view: SplashMvp.View, model: ViewModel?) {
         super.register(view, model)
         firebaseInitInteractor.execute {
-            this.view?.startApp()
+            programsInteractor.downloadAllPrograms {programs ->
+                this.view?.startApp()
+                saveUserProgramsInteractor.programs = programs
+                saveUserProgramsInteractor.execute()
+            }
         }
     }
 }
