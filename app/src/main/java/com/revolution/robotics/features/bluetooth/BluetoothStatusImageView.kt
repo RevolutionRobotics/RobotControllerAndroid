243825d62325b42a225b14ad42d90e223a538a25
László Nagy <laszlo.nagy@supercharge.io>
2019-06-26 11:57:07 +0200

    Changed bluetooth icon color
    RRU-20

--- app/src/main/java/com/revolution/robotics/features/bluetooth/BluetoothStatusImageView.kt
+++ app/src/main/java/com/revolution/robotics/features/bluetooth/BluetoothStatusImageView.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.FragmentActivity
 import com.revolution.robotics.R
-import com.revolution.robotics.core.bindings.setGreyscale
+import com.revolution.robotics.core.bindings.setTintColor
 import org.kodein.di.KodeinAware
 import org.kodein.di.erased.instance
 
@@ -41,6 +41,6 @@ class BluetoothStatusImageView @JvmOverloads constructor(
     }
 
     override fun onBluetoothConnectionStateChanged(connected: Boolean, serviceDiscovered: Boolean) {
-        setGreyscale(this, !connected)
+        setTintColor(this, if (connected) R.color.bluetooth_blue else R.color.white)
     }
 }
