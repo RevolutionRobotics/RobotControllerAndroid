33c078ed6e3f6417c508c9b0c13b7413140f1c8a
Dániel Buga <bugadani@gmail.com>
2020-04-09 09:29:39 +0200

    Explain storing the instances

--- app/src/main/java/com/revolution/robotics/core/interactor/api/DownloadFileInteractorBuilder.kt
+++ app/src/main/java/com/revolution/robotics/core/interactor/api/DownloadFileInteractorBuilder.kt
@@ -10,6 +10,11 @@ class DownloadFileInteractorBuilder(
     private var interactors = HashMap<Pair<URL, String>, DownloadFileInteractor>()
 
     fun createDownloader(src: URL, dst: String): DownloadFileInteractor {
+        // Store the created interactors in a hash map so that a
+        // single interactor belongs to a (source, destination) pair.
+        // This results in a similar solution to having a different interactor instance for
+        // each download, except the instances are created dynamically for cases where one or both
+        // of the file paths are not known in advance.
         return interactors.getOrPut(Pair(src, dst)) {
             DownloadFileInteractor(downloader, src, dst)
         }
