044ea5c68204dfd76b32732dad83055928a590fd
Dániel Buga <bugadani@gmail.com>
2020-04-09 09:26:19 +0200

    Remove WeakReference magic, it has no practical benefits here

--- app/src/main/java/com/revolution/robotics/core/interactor/api/DownloadFileInteractorBuilder.kt
+++ app/src/main/java/com/revolution/robotics/core/interactor/api/DownloadFileInteractorBuilder.kt
@@ -2,27 +2,17 @@ package com.revolution.robotics.core.interactor.api
 
 import com.revolution.robotics.core.kodein.utils.ApplicationContextProvider
 import com.revolution.robotics.core.utils.FileDownloader
-import java.lang.ref.WeakReference
 import java.net.URL
 
 class DownloadFileInteractorBuilder(
     applicationContextProvider: ApplicationContextProvider
 ) {
     private var downloader = FileDownloader(applicationContextProvider)
-    private var interactors = HashMap<Pair<URL, String>, WeakReference<DownloadFileInteractor>>()
+    private var interactors = HashMap<Pair<URL, String>, DownloadFileInteractor>()
 
     fun createDownloader(src: URL, dst: String): DownloadFileInteractor {
-        val key = Pair(src, dst)
-        val weakRef = interactors.getOrPut(key) {
-            WeakReference(DownloadFileInteractor(downloader, src, dst))
+        return interactors.getOrPut(Pair(src, dst)) {
+            DownloadFileInteractor(downloader, src, dst)
         }
-
-        var instance = weakRef.get()
-        if (instance == null) {
-            instance = DownloadFileInteractor(downloader, src, dst)
-            interactors[key] = WeakReference(instance)
-        }
-
-        return instance
     }
 }
