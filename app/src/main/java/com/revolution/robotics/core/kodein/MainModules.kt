f7c082339a6d1b09fc2344f06d8cd27359e001f6
Dániel Buga <bugadani@gmail.com>
2020-04-22 11:23:10 +0200

    Add gson converter to Retrofit

--- app/src/main/java/com/revolution/robotics/core/kodein/MainModules.kt
+++ app/src/main/java/com/revolution/robotics/core/kodein/MainModules.kt
@@ -27,6 +27,7 @@ import org.kodein.di.Kodein
 import org.kodein.di.erased.bind
 import org.revolutionrobotics.bluetooth.android.communication.RoboticsDeviceConnector
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 fun createMainModule() =
@@ -57,7 +58,7 @@ fun createAppModule(context: Context) =
         bind<Reporter>() with s { Reporter(i()) }
         bind<FileManager>() with s { FileManager(context) }
         bind<Retrofit>() with s {
-            val CACHE_SIZE_BYTES: Long = 1024 * 1024 * 2;
+            val CACHE_SIZE_BYTES: Long = 1024 * 1024 * 2
             val builder = OkHttpClient().newBuilder()
             builder.cache(
                 Cache(context.cacheDir, CACHE_SIZE_BYTES)
@@ -67,6 +68,7 @@ fun createAppModule(context: Context) =
             retrofitBuilder
                 .baseUrl(BuildConfig.BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
             retrofitBuilder.build()
 
