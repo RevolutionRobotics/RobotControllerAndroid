0af9442e6247922f230cfd3a07ad85dea348c94d
Dániel Buga <bugadani@gmail.com>
2020-04-22 11:39:55 +0200

    Use cached data if request fails

--- app/src/main/java/com/revolution/robotics/core/api/RoboticsService.kt
+++ app/src/main/java/com/revolution/robotics/core/api/RoboticsService.kt
@@ -3,6 +3,7 @@ package com.revolution.robotics.core.api
 import com.revolution.robotics.core.domain.remote.VersionData
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 
 interface RoboticsService {
@@ -20,6 +21,7 @@ interface RoboticsService {
     @GET("v$API_VERSION//firmware")
     fun getFirmware(): Call<String>
 
+    @Headers("Cache-Control: public, only-if-cached, max-stale=604800, stale-if-error=604800")
     @GET("v$API_VERSION//versionData")
     fun getVersionData(): Call<VersionData>
 }
